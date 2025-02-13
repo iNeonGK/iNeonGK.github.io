0f4d147cd22455136cf4b52d2e3ac6063e8d2335
iNeonGK <aass78421@gmail.com>
2025-02-13 17:47:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 2) % messages.length;
+    messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
